8314c66792ee2a7ab606020ecf5b7e4f17ebee2f
BryanPF <bryanpaz26@gmail.com>
2022-12-26 18:53:40 -0600

    primer commit

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,8 +1,41 @@
 import './mylabel.css';
 
 
-export const MyLabel = () => {
+export interface MyLabelProps {
+  /**
+   * Este es el mensaje que se va mostrar en el label
+   */
+  label: string;
+  /**
+   * Este es el tamaño  del label
+   */
+  size: 'normal'|'h1'|'h2'|'h3';
+  /**
+   * Si quiere todo capitalizado
+   */
+  allCaps?: boolean;
+  /**
+   * Colores basicos del boton
+   */
+  color?: 'primary'|'secondary'|'tertiary';
+  /**
+   * Color personalizado de la fuente
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ( {  
+    allCaps = false, 
+    color = 'primary', 
+    label = 'No Label', 
+    size = 'normal',
+    fontColor, 
+}: MyLabelProps ) => {
+
   return (
-    <span>Hola mundo</span>
+    <span className={`label ${ size } text-${ color }`} style={{ color: fontColor }}>
+      { allCaps ? label.toUpperCase() : label }
+    </span>
   )
+
 }
